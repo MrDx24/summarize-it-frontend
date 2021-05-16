d08d7b2f9732877f033d49479633fd6d54719fa9
Deep Patadiya <deeppatadiya24@gmail.com>
2021-05-16 23:22:00 +0530

    updated files

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/summarizer'));
+app.use(express.static(__dirname + './dist/summarizer'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/summarizer/'}),
+    res.sendFile(path.join(__dirname + '/dist/summarizer/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
